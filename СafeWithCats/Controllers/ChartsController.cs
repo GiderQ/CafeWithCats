e4a50355dc9dee4516704057a7826498305d4897
Yaroslav Davydenko <hollebeen@gmail.com>
2026-04-05 20:43:42 +0300

    1.5

---
+++ СafeWithCats/Controllers/ChartsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CafeWithCats.Models;
+
+namespace CafeWithCats.Controllers;
+
+public class ChartsController : Controller
+{
+    private readonly CafeContext _context;
+
+    public ChartsController(CafeContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var drinks = await _context.Drinks.ToListAsync();
+
+        ViewBag.DrinkNames = drinks.Select(d => d.Name).ToList();
+        ViewBag.DrinkPrices = drinks.Select(d => d.Price).ToList();
+
+        return View();
+    }
+}
